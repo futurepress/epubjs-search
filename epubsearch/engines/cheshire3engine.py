73d3e8558e6729912eecc22fb049d27aac50894a
munners17 <munners@berkeley.edu>
2013-12-19 19:28:40 -0800

    Added a manual implementation of the search term highlight markup

--- epubsearch/engines/cheshire3engine.py
+++ epubsearch/engines/cheshire3engine.py
@@ -1,11 +1,13 @@
 import os
-import sys
+import re
+import pdb
 from baseengine import BaseEngine
 from cheshire3.baseObjects import Session
 from cheshire3.server import SimpleServer
 from cheshire3.internal import cheshire3Root
+from cheshire3.exceptions import ObjectDoesNotExistException
 import json
-import pdb
+
 
 class Cheshire3Engine(BaseEngine):
     #schema = Schema(title=TEXT(stored=True), path=TEXT(stored=True), href=ID(stored=True), cfiBase=TEXT(stored=True), spinePos=TEXT(stored=True), content=TEXT)
@@ -16,29 +18,53 @@ class Cheshire3Engine(BaseEngine):
     server = SimpleServer(session, serverConfig)
     queryFactory = None
     db = None
-    sel = None
+    titleSel = None
+    anywhereSel = None
+    proxExtractor = None
 
-
-    def __initializeSelector(self):
-
+    def __initializeTitleSelector(self):
         try:
-            self.sel = self.db.get_object(self.session, 'titleXPathSelector')
+            self.titleSel = self.db.get_object(self.session, 'titleXPathSelector')
         except ObjectDoesNotExistException:
             try:
-                self.sel = self.db.get_object(self.session, 'titleSelector')
-            except ObjectDoesNotExistException:
-                pass
+                self.titleSel = self.db.get_object(self.session, 'titleSelector')
+            except ObjectDoesNotExistException as e:
+                print e
+
+    def __initializeAnywhereSelector(self):
+        try:
+            self.anywhereSel = self.db.get_object(self.session, 'anywhereXPathSelector')
+        except ObjectDoesNotExistException as e:
+            print e
+
+    def __initializeProximityExtractor(self):
+        try:
+            self.proxExtractor = self.db.get_object(self.session, 'ProxExtractor')
+        except ObjectDoesNotExistException as e:
+            print e
+
+    def __highlight(self, text, term, n):
+        """Searches for text, retrieves n words either side of the text, which are retuned seperately"""
+        term_concordance = list()
+        text_len = len(text)
+        term_len = len(term)
+        term_indexes = [w.start() for w in re.finditer(term, text)]
+        for idx in term_indexes:
+            start = idx - n
+            end = text_len if (idx + term_len + n) > text_len else idx + term_len + n
+            term_concordance.append(text[start:idx] + '<b class="match term0">' + term + '</b>' + text[idx:end]) 
+
+        return term_concordance
 
     def open(self):
         """ The Cheshire get_object line should throw an exception if it can't 
         open passed db
         """
-        
-        #pdb.set_trace()
         try:
             self.db = self.server.get_object(self.session, self.databaseName)
             self.session.database = self.databaseName
-        except Exception, e:
+        except Exception as e:
+            print e
             print "openning database {} failed".format(self.databaseName)
 
     def create(self):
@@ -83,14 +109,20 @@ class Cheshire3Engine(BaseEngine):
         pass
 
     def query(self, q, limit=None):
-        """ In Cheshire, the
+        """ In Cheshire3, you have to specify an index and query, else it defaults the all index  which utilizes simple extraction.
         """
 
         if self.queryFactory == None:
             self.queryFactory = self.db.get_object(self.session, 'defaultQueryFactory')
 
-        if self.sel is None:
-            self.__initializeSelector()
+        if self.titleSel is None:
+            self.__initializeTitleSelector()
+
+        if self.anywhereSel is None:
+            self.__initializeAnywhereSelector()
+
+        if self.proxExtractor is None:
+            self.__initializeProximityExtractor()
 
         c3Query = self.queryFactory.get_query(self.session, q)
         rs = self.db.search(self.session, c3Query)
@@ -102,14 +134,20 @@ class Cheshire3Engine(BaseEngine):
 
         # loop through recordset, create new results list with dictionary of found values
         results = list()
-        for rs_ins in rs:
-            rec = rs_ins.fetch_record(self.session)
+        for rsi in rs:
+            rec = rsi.fetch_record(self.session)
             # check the record titles
-            titleData = self.sel.process_record(self.session, rec)
+            titleData = self.titleSel.process_record(self.session, rec)
+            # checking out the proximity attributes
+            elems = self.anywhereSel.process_record(self.session, rec)           
+            doc_dict = self.proxExtractor.process_xpathResult(self.session, elems).values()[0]
+            concordance = self.__highlight(doc_dict['text'], q, 20)
+            pdb.set_trace()
+            # extracts document name key
             fn_key = os.path.basename(titleData[3][0])
-            #pdb.set_trace()
+            # append highlighted concordance to the dictionary
+            db_md_dict[fn_key][u'highlight'] = "  ".join(concordance)
             results.append(db_md_dict[fn_key])
-
         return results
 
 
